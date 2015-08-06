82240520d6d28764222b08da47e1039b18f3663f
Justin Scott & Lisa Duschek <justinandlisa@fake.com>
2015-08-06 12:05:38 -0700

    It should have spec to return user input in quarters, dimes and nickels

--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -6,10 +6,12 @@ describe('Fixnum#combine') do
     expect((25).combine).to(eq('1 quarters, 0 dimes, 0 nickels, 0 pennies'))
   end
 
-it('takes user input and returns the amount of quarters and dimes') do
-  expect((35).combine).to(eq('1 quarters, 1 dimes, 0 nickels, 0 pennies'))
-end
+  it('takes user input and returns the amount of quarters and dimes') do
+    expect((35).combine).to(eq('1 quarters, 1 dimes, 0 nickels, 0 pennies'))
+  end
 
-
+  it('takes user input and returns the amount of quarters, dimes and nickels') do
+    expect((40).combine).to(eq('1 quarters, 1 dimes, 1 nickels, 0 pennies'))
+  end
 
 end
