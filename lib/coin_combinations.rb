7f772ea0371653b6c064480c56239978a18a9bb1
Justin Scott & Lisa Duschek <justinandlisa@fake.com>
2015-08-06 11:52:03 -0700

    It should return the amount of quarters in the users input

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,5 +1,21 @@
+require('pry')
+
 class Fixnum
   define_method(:combine) do
-    self
+    change = self
+    pennies = 0
+    nickels = 0
+    dimes = 0
+    quarters = 0
+    output = ''
+
+    until change.<(25)
+      if change.>=(25)
+        change = change.-(25)
+        quarters = quarters.+(1)
+        output.concat(quarters.to_s()).concat(' quarters')
+      end
+    end
+    output
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,12 +2,12 @@ require ('rspec')
 require('./lib/coin_combinations')
 
 describe('Fixnum#combine') do
-  it('takes user input and returns pennies for change') do
-    expect((10).combine).to(eq(10))
+  it('takes user input and returns the amount of quarters') do
+    expect((25).combine).to(eq('1 quarters'))
   end
 
-  it('takes user input and returns nickels for change') do
-    expect((10).combine).to(eq(2))
-  end
+  # it('takes user input and returns nickels for change') do
+  #   expect((10).combine).to(eq(2))
+  # end
 
 end
