ad9e95c48c070912e19aaa4ad89f169d541dfb89
Justin Scott & Lisa Duschek <justinandlisa@fake.com>
2015-08-06 10:17:12 -0700

    It takes user input and returns amount in nickels

--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -5,4 +5,9 @@ describe('Fixnum#combine') do
   it('takes user input and returns pennies for change') do
     expect((10).combine).to(eq(10))
   end
+
+  it('takes user input and returns nickels for change') do
+    expect((10).combine).to(eq(2))
+  end
+
 end
